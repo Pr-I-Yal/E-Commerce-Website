424351bb3ea082ef098fb2145a3443c4daf5f0d2
Pr-I-Yal <harshitapriyal168@gmail.com>
2026-08-11 12:38:07 +0530

    10/08/2026 Shop DropDown ,Search , Latest

--- src/Components/SingleProduct.jsx
+++ src/Components/SingleProduct.jsx
@@ -6,11 +6,20 @@ export default function SingleProduct({ item }) {
         <div className="team-item position-relative overflow-hidden">
             <img className="img-fluid w-100" src={`${import.meta.env.VITE_APP_IMAGE_SERVER}${item.pic[0]}`} style={{ height: 350 }} alt="" />
             <div className="team-overlay">
-                <small className="mb-2">{item.brand}</small>
-                <h6 className="lh-base text-light">{item.name}</h6>
-                <h5 className='text-light'><del>&#8377;{item.basePrice}</del> &#8377;{item.finalPrice}<sup>{item.discount}% Off</sup></h5>
-                <div className="d-flex justify-content-center">
-                    <Link className="btn btn-outline-light btn-sm-square border-2 p-2 w-100" to={`product/${item.id}`}>
+                <div className="position-absolute bottom-0 start-0 m-3 d-flex">
+                    <small className="px-3 py-2 bg-success text-light">{item.brand}</small>
+                    <small className="px-3 py-2 bg-success text-light">{item.stockQuantity} Left in Stock</small>
+                </div>
+                <h6 className="position-absolute top-0 start-0 m-3 px-2 py-1 text-light bg-dark bg-opacity-75 rounded">{item.name}</h6>
+                <div className="position-absolute bottom-0 start-0 mb-5 ms-3">
+                    <h5 className='text-light mb-2'>
+                        <del>&#8377;{item.basePrice}</del>
+                        <span className="ms-2">&#8377;{item.finalPrice}</span>
+                        <sup className="ms-2 text-warning">{item.discount}% Off</sup>
+                    </h5>
+                </div>
+                <div className="position-absolute bottom-0 end-0 m-3">
+                    <Link className="btn btn-outline-light border-2 px-3 py-2" to={`product/${item.id}`}>
                         <i className="text-success bi bi-cart-plus"></i>
                         <span className='ms-2'>Add to Cart</span>
                     </Link>
--- src/Pages/ShopPage.jsx
+++ src/Pages/ShopPage.jsx
@@ -1,8 +1,7 @@
 import React, { useEffect, useState } from 'react'
-import BreadCrum from '../Components/BreadCrum'
-
 import { useDispatch, useSelector } from 'react-redux'
 
+import BreadCrum from '../Components/BreadCrum'
 import SingleProduct from '../Components/SingleProduct'
 
 import { getProduct } from "../Redux/ActionCreators/ProductActionCreators"
@@ -10,8 +9,22 @@ import { getMaincategory } from "../Redux/ActionCreators/MaincategoryActionCreat
 import { getSubcategory } from "../Redux/ActionCreators/SubcategoryActionCreators"
 import { getBrand } from "../Redux/ActionCreators/BrandActionCreators"
 
+const colors = ["Black", "White", "Blue", "Red", "Orange", "Gray", "Green", "Pink", "Yellow", "Purple", "Magenta", "N/A"]
+const sizes = ["XXXL", "XXL", "XL", "L", "M", "S", "XS", "NB", "26", "28", "30", "32", "34", "36", "38", "40", "42", "N/A"]
+
 export default function ShopPage() {
     let [data, setData] = useState([])
+
+    let [selected, setSelected] = useState({
+        maincategory: [],
+        subcategory: [],
+        brand: [],
+        color: [],
+        size: []
+    })
+
+    let [sortFilter, setSortFilter] = useState("1")
+    let [search, setSearch] = useState("")
 
     let MaincategoryStateData = useSelector(state => state.MaincategoryStateData)
     let SubcategoryStateData = useSelector(state => state.SubcategoryStateData)
@@ -19,6 +32,52 @@ export default function ShopPage() {
     let ProductStateData = useSelector(state => state.ProductStateData)
 
     let dispatch = useDispatch()
+
+    function getInput(key, value) {
+        let arr = selected[key]
+        if (arr.includes(value))
+            arr = arr.filter(x => x !== value)
+        else
+            arr.push(value)
+
+        setSelected({ ...selected, [key]: arr })
+        filter({ ...selected, [key]: arr })
+    }
+
+    function filter(selected) {
+        let data = ProductStateData.filter(x => x.status && (
+            (selected.maincategory?.length === 0 || selected?.maincategory?.includes(x.maincategory)) &&
+            (selected.subcategory?.length === 0 || selected?.subcategory?.includes(x.subcategory)) &&
+            (selected.brand?.length === 0 || selected?.brand?.includes(x.brand)) &&
+            (selected.color?.length === 0 || new Set(selected?.color).intersection(new Set(x.color)).size > 0) &&
+            (selected.size?.length === 0 || new Set(selected?.size).intersection(new Set(x.size)).size > 0)
+        ))
+        applySortFilter(data, sortFilter)
+    }
+
+    function applySortFilter(data, value) {
+        if (value === "1")
+            setData(data.sort((x, y) => y.id.localeCompare(x.id)))
+        else if (value === "2")
+            setData(data.sort((x, y) => x.finalPrice - y.finalPrice))
+        else
+            setData(data.sort((x, y) => y.finalPrice - x.finalPrice))
+
+        setSortFilter(value)
+    }
+
+    function postSearch() {
+        let ch = search.toLocaleLowerCase()
+        let data = ProductStateData.filter(x => x.status && (
+            (x.name.toLocaleLowerCase().includes(ch)) ||
+            (x.maincategory.toLocaleLowerCase === ch) ||
+            (x.subcategory.toLocaleLowerCase === ch) ||
+            (x.brand.toLocaleLowerCase === ch) ||
+            (x.color.find(p => p.toLocaleLowerCase() === ch))
+        ))
+
+        applySortFilter(data, sortFilter)
+    }
 
     useEffect(() => {
         (() => dispatch(getMaincategory()))()
@@ -47,11 +106,84 @@ export default function ShopPage() {
 
             <div className="container-fluid my-3">
                 <div className="row">
-                    <div className="col-md-3"></div>
+                    <div className="col-md-3">
+
+                        <ul className="list-group mb-3">
+                            <li className="list-group-item active" aria-current="true">Maincategory</li>
+                            {MaincategoryStateData.filter(x => x.status).map(item => {
+                                return <li className="list-group-item" key={item.id} onClick={() => getInput('maincategory', item.name)}>
+                                    <span>{item.name}</span>
+                                    {selected.maincategory?.includes(item.name) ? <i className='bi bi-check float-end'></i> : null}
+                                </li>
+                            })}
+                        </ul>
+
+                        <ul className="list-group mb-3">
+                            <li className="list-group-item active" aria-current="true">Subcategory</li>
+                            {SubcategoryStateData.filter(x => x.status).map(item => {
+                                return <li className="list-group-item" key={item.id} onClick={() => getInput('subcategory', item.name)}>
+                                    <span>{item.name}</span>
+                                    {selected.subcategory?.includes(item.name) ? <i className='bi bi-check float-end'></i> : null}
+                                </li>
+                            })}
+                        </ul>
+
+                        <ul className="list-group mb-3">
+                            <li className="list-group-item active" aria-current="true">Brands</li>
+                            {BrandStateData.filter(x => x.status).map(item => {
+                                return <li className="list-group-item" key={item.id} onClick={() => getInput('brand', item.name)}>
+                                    <span>{item.name}</span>
+                                    {selected.brand?.includes(item.name) ? <i className='bi bi-check float-end'></i> : null}
+                                </li>
+                            })}
+                        </ul>
+
+                        <ul className="list-group mb-3">
+                            <li className="list-group-item active" aria-current="true">Colors</li>
+                            {colors.map((item, index) => {
+                                return <li className="list-group-item" key={index} onClick={() => getInput('color', item)}>
+                                    <span>{item}</span>
+                                    {selected.color?.includes(item) ? <i className='bi bi-check float-end'></i> : null}
+                                </li>
+                            })}
+                        </ul>
+
+                        <ul className="list-group mb-3">
+                            <li className="list-group-item active" aria-current="true">Sizes</li>
+                            {sizes.map((item, index) => {
+                                return <li className="list-group-item" key={index} onClick={() => getInput('size', item)}>
+                                    <span>{item}</span>
+                                    {selected.size?.includes(item) ? <i className='bi bi-check float-end'></i> : null}
+                                </li>
+                            })}
+                        </ul>
+
+                    </div>
                     <div className="col-md-9">
                         <div className="row">
+                            <div className="col-xl-9 col-md-6 mb-3">
+                                <form onSubmit={(e) => {
+                                    e.preventDefault()
+                                    postSearch()
+                                }}>
+                                    <div className="btn-group w-100">
+                                        <input type="search" name="search" onChange={(e) => setSearch(e.target.value)} placeholder='Search Products By Name, Category, Brand or Color etc' className='form-control' />
+                                        <button className='btn btn-primary' onSubmit={postSearch}>Search</button>
+                                    </div>
+                                </form>
+                            </div>
+                            <div className="col-xl-3 col-md-6 mb-3">
+                                <select className='form-select' onChange={(e) => applySortFilter(data, e.target.value)}>
+                                    <option value="1">Latest</option>
+                                    <option value="2">Price : Low to High</option>
+                                    <option value="3">Price : High to Low</option>
+                                </select>
+                            </div>
+                        </div>
+
+                        <div className="row">
                             {data.map(item => {
-                                return <div key={item.id} className="col-md-6 col-lg-4 wow fadeIn" data-wow-delay="0.1s">
+                                return <div key={item.id} className="col-md-6 col-lg-4">
                                     <SingleProduct item={item} />
                                 </div>
                             })}
